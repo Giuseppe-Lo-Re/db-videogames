b551b57240714513c23ef82bae51e5b153f7ff5c
Giuseppe Lo Re <loregiuseppe0@gmail.com>
2022-11-14 14:59:55 +0100

    10 domande completate

--- query-videogiochi.sql
+++ query-videogiochi.sql
@@ -1,35 +1,68 @@
 --QUERY SELECT
 
 --1- Selezionare tutte le software house americane (3)
---select * 
---from software_houses
---where country = 'United States';
+
+SELECT * 
+FROM software_houses
+WHERE country = 'United States';
 
 --2- Selezionare tutti i giocatori della città di 'Rogahnland' (2)
---select * 
---from players
---where city = 'Rogahnland';
+
+SELECT * 
+FROM players
+WHERE city = 'Rogahnland';
 
 --3- Selezionare tutti i giocatori il cui nome finisce per "a" (220)
---select * 
---from players
---where name like '%a';
+
+SELECT * 
+FROM players
+WHERE name LIKE '%a';
+
 --4- Selezionare tutte le recensioni scritte dal giocatore con ID = 800 (11)
-select *
-from reviews
-where player_id = '800';
+
+SELECT *
+FROM reviews
+WHERE player_id = '800';
 
 --5- Contare quanti tornei ci sono stati nell'anno 2015 (9)
 
+SELECT *
+FROM tournaments
+WHERE year = '2015';
+
 --6- Selezionare tutti i premi che contengono nella descrizione la parola 'facere' (2)
+
+SELECT *
+FROM awards
+WHERE description LIKE '%facere%';
 
 --7- Selezionare tutti i videogame che hanno la categoria 2 (FPS) o 6 (RPG), mostrandoli una sola volta (del videogioco vogliamo solo l'ID) (287)
 
+SELECT videogame_id 
+FROM category_videogame
+WHERE category_id = 2 OR category_id = 6
+GROUP BY videogame_id;
+
 --8- Selezionare tutte le recensioni con voto compreso tra 2 e 4 (2947)
+
+SELECT *
+FROM reviews
+WHERE rating >= 2 AND rating <= 4;
 
 --9- Selezionare tutti i dati dei videogiochi rilasciati nell'anno 2020 (46)
 
+SELECT *
+FROM videogames
+WHERE DATEPART(year, release_date) = 2020;
+
 --10- Selezionare gli id dei videogame che hanno ricevuto almeno una recensione da stelle, mostrandoli una sola volta (443)
+
+SELECT videogame_id, COUNT(videogame_id) AS counter
+FROM reviews
+WHERE rating = 5
+GROUP BY videogame_id
+
+
 
 --*********** BONUS ***********
 
